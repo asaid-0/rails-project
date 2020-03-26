16d12bbddcb2eb95981c8ae3fe537e6f82d430bf
asaid-0 <60660214+asaid-0@users.noreply.github.com>
2020-03-26 21:37:28 +0200

    Added default user when migration executed

--- db/migrate/20200325093208_create_users.rb
+++ db/migrate/20200325093208_create_users.rb
@@ -1,7 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
-  def change
+  def up
     create_table :users do |t|
       t.string :name
     end
+
+    User.create :name => "Ahmed"
   end
+
+  def down
+    drop_table :users
+  end
+
 end
